39b7ac449dc2cbab8374127721b6fca8521a0a02
ashwini607 <ashwinikmr44@gmail.com>
2014-02-05 16:58:47 +0000

    tool improvement

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -55,10 +55,12 @@ public class Application extends Controller {
 
 
 	public static String mixQuery;
+ 
 
-
-	public static void ChEMBL(String details, String firstname, String unit, String value, String journal, String date, String phase, String type, String score){
+	public static void ChEMBL(String details, String firstname, String unit, String value, String journal, String date, String phase, String type, String score, String uni, String dis){
+		
 		String organism = new String();
+		String mixQuery = new String();
 
 		if (firstname.length()>0){
 			organism = "FILTER regex(?organism,'" + firstname + "', 'i')";
@@ -74,7 +76,7 @@ public class Application extends Controller {
 		if (value.length()>0){
 			values = "FILTER (?stdValue" +value+ ")";
 		}
-		
+
 		String journals = new String();		
 		if (journal.length()>0){
 			journals = "FILTER regex(?journalName,'" +journal+ "', 'i')";
@@ -84,22 +86,28 @@ public class Application extends Controller {
 		if (date.length()>0){
 			dates = "FILTER (?date" +date+ ")";
 		}
-		
+
 		String phases = new String();		
 		if (phase.length()>0){
 			phases = "FILTER (?highestDevelopmentPhase" +phase+ ")";
 		}
-		
+
 		String types = new String();		
 		if (type.length()>0){
 			types = "FILTER regex(?assayType,'" +type+ "', 'i')";
 		}
-		
+
 		String scores = new String();		
 		if (score.length()>0){
 			scores = "FILTER (?targetConfScore" +score+ ")";
 		}
 		
+		String diss = new String();		
+		if (dis.length()>0){
+			diss = "FILTER regex(?disease,'" +dis+ "', 'i'"
+					+ ")";
+		}
+
 		List <String> myList = new ArrayList(); 
 		List <String> myList1 = new ArrayList();
 		List <String> myList2 = new ArrayList();
@@ -130,13 +138,16 @@ public class Application extends Controller {
 		List <String> myList27 = new ArrayList();
 		List <String> myList28 = new ArrayList();
 		List <String> myList29 = new ArrayList();
+		List <String> myList30 = new ArrayList();
 
-		String selection = "  ?journalName ?date ?stdType ?stdValue ?stdRelation ?stdUnit ?protein ?annotation ?pathwayname ?ChEMBL_id ?moleculeDesc ?altLabel ?highestDevelopmentPhase ?substanceType ?assayLabel ?assayDesc ?assayType ?targetConfDesc ?targetConfScore ?targetRelType ?targetRelDesc ?targetLabel ?targetTitle ?targetType ?organism ?disease";
+		String selection = "?protein ?recommended ?disease ?aa";
 		String addSelection =  new String();
-		String[] finalSelection = selection.split(" ");
+		String addSelection1 =  new String();
+		
+		if(details != null){
+			String selection1 = "?journalName ?date ?stdType ?stdValue ?stdRelation ?stdUnit ?annotation ?pathwayname ?ChEMBL_id ?moleculeDesc ?altLabel ?highestDevelopmentPhase ?substanceType ?assayLabel ?assayDesc ?assayType ?targetConfDesc ?targetConfScore ?targetRelType ?targetRelDesc ?targetLabel ?targetTitle ?targetType ?organism ";
+		String[] finalSelection = selection1.split(" ");
 		String[] finalDetails = details.split(" ");
-		System.out.println(finalSelection.length);
-		System.out.println(finalDetails.length);
 
 		for(int item1=0; item1<finalDetails.length; item1++) 
 		{
@@ -168,10 +179,67 @@ public class Application extends Controller {
 		}
 
 		details = details.replaceAll(",", "");
-		System.out.println(addSelection);
+		}	
+		
+		if(uni != null){
+		String[] finalSelection1 = selection.split(" ");
+		String[] finalDetails1 = uni.split(" ");
+
+		for(int item1=0; item1<finalDetails1.length; item1++) 
+		{
+			if(finalDetails1[item1].contains("."))
+			{finalDetails1[item1]=(finalDetails1[item1].replaceAll("\\.", ""));}
+			if(finalDetails1[item1].contains(".,"))
+			{finalDetails1[item1]=(finalDetails1[item1].replaceAll("\\.,", ""));}
+			if(finalDetails1[item1].contains(","))
+			{finalDetails1[item1]=(finalDetails1[item1].replaceAll(",", ""));}
+
+		}		
+
+
+		int item;
+		//item2 = 0;
+		for ( item=0; item<finalSelection1.length; item++)
+		{
+			for(int item1=0; item1<finalDetails1.length; item1++) 
+			{
+				if(finalSelection1[item].equals(finalDetails1[item1]))
+
+				{ addSelection1 += " "+finalSelection1[item];
+				break;
+				}
+
+			}
+
+
+		}
+
+		uni = uni.replaceAll(",", "");
+		}	
+		
+	String uniprotQuery = new String();
+	uniprotQuery = "SERVICE <http://beta.sparql.uniprot.org/sparql> {" +
+	uni +
+	diss +
+	"}\n"
+	      ;
+			
+		
+	if(details==null){	
 		mixQuery = addFirst + "SELECT DISTINCT"+
-				addSelection+
-				" WHERE { \n" +
+				addSelection +
+				addSelection1 +
+				" WHERE { \n" +			    
+				uniprotQuery +
+				"}\n"+
+				"LIMIT 11";}
+	
+	
+	if(uni==null){	
+		mixQuery = addFirst + "SELECT DISTINCT"+
+				addSelection +
+				addSelection1 +
+				" WHERE { \n" +		
 				details +
 				organism +	
 				units +
@@ -181,9 +249,28 @@ public class Application extends Controller {
 				phases +
 				types +
 				scores +
-
+				
 				"}\n"+
-				"LIMIT 11";
+				"LIMIT 11";}
+	
+	
+	if(uni!=null && details!=null){	
+		mixQuery = addFirst + "SELECT DISTINCT"+
+				addSelection +
+				addSelection1 +
+				" WHERE { \n" +		
+				details +
+				organism +	
+				units +
+				values +
+				journals +
+				dates +
+				phases +
+				types +
+				scores +
+				uniprotQuery+
+				"}\n"+
+				"LIMIT 11";}
 		//mixQuery = mixQuery.replaceAll(",", "");
 
 		System.out.println(mixQuery);
@@ -241,6 +328,7 @@ public class Application extends Controller {
 			RDFNode w = soln.get("organism") ; 
 
 			RDFNode a1 = soln.get("disease") ; 
+			RDFNode b1 = soln.get("aa") ;
 			RDFNode aa = soln.get("document") ; 
 			if(aa != null)
 				myList27.add(aa.toString());
@@ -310,11 +398,13 @@ public class Application extends Controller {
 				myList25.add(w.toString());		
 			if(a1 != null)
 				myList26.add(a1.toString());
+			if(b1 != null)
+				myList30.add(b1.toString());
 
 		}
 		qExe.close();
 
-		render(myList,myList1,myList2,myList3,myList4,myList5,myList6,myList7,myList8,myList9,myList10,myList11,myList12,myList13,myList14,myList15,myList16,myList17,myList18,myList19,myList20,myList21,myList22,myList23,myList24,myList25, myList26, myList27, myList28, myList29);
+		render(myList,myList1,myList2,myList3,myList4,myList5,myList6,myList7,myList8,myList9,myList10,myList11,myList12,myList13,myList14,myList15,myList16,myList17,myList18,myList19,myList20,myList21,myList22,myList23,myList24,myList25, myList26, myList27, myList28, myList29, myList30);
 
 
 	}
@@ -341,6 +431,11 @@ public class Application extends Controller {
 	public static void home() {
 		render();		
 	}
+	
+	public static void example() {
+		render();		
+	}
+	
 }
 
 
